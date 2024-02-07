5b832340e938c889d930659f73a871e3b12871e3
mostaphaelansari <138169519+mostaphaelansari@users.noreply.github.com>
2024-02-08 00:22:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import cv2
+import torch
+import numpy as np
+from tracker import *
+
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+cap=cv2.VideoCapture('highway.mp4')
+
+count=0
+tracker = Tracker()
+
+
+
+
+def POINTS(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = [x, y]
+        print(colorsBGR)
+        
+
+cv2.namedWindow('FRAME')
+cv2.setMouseCallback('FRAME', POINTS)
+
+while True:
+    ret,frame=cap.read()
+    if not ret:
+        break
+    count += 1
+    if count % 3 != 0:
+        continue
+    frame=cv2.resize(frame,(1020,600))
+    results=model(frame)
+    results.pandas().xyxy[0]
+       
+        
+    
+    cv2.imshow("FRAME",frame)
+    if cv2.waitKey(0)&0xFF==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
